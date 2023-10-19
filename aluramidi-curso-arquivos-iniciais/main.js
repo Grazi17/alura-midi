1322a656ae3da97191ba18103b7dbcd6b07bb26a
Grazieli Geovana Haas <117859665+Grazi17@users.noreply.github.com>
2023-10-19 11:00:55 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,13 @@
+alert('Essa pagina contem virus' );
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play();};
+
+document.querySelector('.tecla_pom').onclick=tocaSompom ;
+document.querySelector('.tecla_clap');
+document.querySelector('.tecla_tim');
+document.querySelector('.tecla_puff') ;
+document.querySelector('.tecla_splash');
+document.querySelector('.tecla_toim') ;
+document.querySelector('.tecla_psh') ;
+document.querySelector('.tecla_tic') ;
+document.querySelector('.tecla_tom') ;
+
