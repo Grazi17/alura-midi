b54a22490e79573f609e1dc952fdfbfd68f386f8
Grazieli Geovana Haas <117859665+Grazi17@users.noreply.github.com>
2023-10-26 11:00:00 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,13 +1,30 @@
 alert('Essa pagina contem virus' );
+
 function tocaSomPom(){document.querySelector('#som_tecla_pom').play();};
-
 document.querySelector('.tecla_pom').onclick=tocaSompom ;
-document.querySelector('.tecla_clap');
-document.querySelector('.tecla_tim');
-document.querySelector('.tecla_puff') ;
-document.querySelector('.tecla_splash');
-document.querySelector('.tecla_toim') ;
-document.querySelector('.tecla_psh') ;
-document.querySelector('.tecla_tic') ;
-document.querySelector('.tecla_tom') ;
 
+function tocaSomClap(){document.querySelector('#som_tecla_clap').play();};
+document.querySelector('.tecla_clap').onclick=tocaSomclap;
+
+function tocaSomTim(){document.querySelector('#som_tecla_tim').play();};
+document.querySelector('.tecla_tim').onclick=tocaSomtim;
+
+function tocaSomPuff(){document.querySelector('#som_tecla_puff').play();};
+document.querySelector('.tecla_puff').onclick=tocaSompuff ;
+
+function tocaSomSplash(){document.querySelector('#som_tecla_splash').play();};
+document.querySelector('.tecla_splash').onclick=tocaSomsplash;
+
+function tocaSomToim(){document.querySelector('#som_tecla_toim').play();};
+document.querySelector('.tecla_toim').onclick=tocaSomtoim ;
+
+function tocaSomPsh(){document.querySelector('#som_tecla_psh').play();};
+document.querySelector('.tecla_psh').onclick=tocaSompsh;
+
+function tocaSomTic(){document.querySelector('#som_tecla_tic').play();};
+document.querySelector('.tecla_tic').onclick=tocaSomtic;
+
+function tocaSomTom(){document.querySelector('#som_tecla_tom').play();};
+document.querySelector('.tecla_tom').onclick=tocaSomtom ;
+
+const Listadeteclas=document.querySelectorAll('.tecla')
